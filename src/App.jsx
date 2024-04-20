67767a6b5f9a995541e9691406c41983b486ac3b
sanjeetgit1 <sksanjeet616@gmail.com>
2024-04-21 01:56:00 +0530

    first

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,12 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  
+  console.log(import.meta.env.VITE_APP_APPWRITE_URL)
 
   return (
     <>
       <h1>hello my new blog project</h1>
-      <h1>ssss</h1>
+      <h1>hhhhhhhhhhh</h1>
     </>
   )
 }
