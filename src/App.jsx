fa5e5a03f496ed5ebbaa7981c2c95b40037c46d6
sanjeetgit1 <sksanjeet616@gmail.com>
2024-04-21 01:06:42 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>hello my new blog project</h1>
+      <h1>ssss</h1>
+    </>
+  )
+}
+
+export default App
