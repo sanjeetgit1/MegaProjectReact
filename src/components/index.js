8ba353c02aba142f990292174012e3cd6db7493a
sanjeetgit1 <sksanjeet616@gmail.com>
2024-05-04 16:01:36 +0530

    adding from and slug

--- src/components/index.js
+++ src/components/index.js
@@ -3,10 +3,13 @@ import Footer from './Footer/Footer.jsx'
 import Container from './container/container.jsx'
 import Logo from './logo.jsx'
 import LogoutBtn from './Header/LogoutBtn.jsx'
+import RTE from './RTE.jsx'
+
 export{
     Header,
     Footer,
     Container,
     Logo,
     LogoutBtn,
+    RTE,
 }
